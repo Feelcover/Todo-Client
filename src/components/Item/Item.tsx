87b039bee3a2634f496d4dbcfcc10f94add9fb1d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-19 00:57:16 +0300

    стили

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import styles from './Item.module.css'
+import React from "react";
+import styles from "./Item.module.css";
 
 const Item = () => {
   return (
     <li>
-        <div>
-            <span></span>
-        </div>
+      <div>
+        <span></span>
+      </div>
+      <div>
+        <button className="btn btn-primary">Изменить</button>
+        <button className="btn btn-success">Завершить</button>
+        <button className="btn btn-danger">Удалить</button>
+      </div>
     </li>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
