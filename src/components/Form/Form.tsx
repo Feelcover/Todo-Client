4f478dc121dff0ec17b294bbe7114778cd9ae2d3
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-19 14:27:08 +0300

    store и подключение стейта в form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import styles from "./Form.module.css"
+import { useSelector } from "react-redux";
+import styles from "./Form.module.css";
 
 const Form = () => {
+  const state = useSelector((state: any) => state.todoReducer);
+
   return (
     <form className={styles.form}>
-        <div className={styles.container}>
-            <label className='form-label' htmlFor="">Введите название задачи</label>
-            <input type="text" className='form-control' />
-        </div>
-        <button className={`${styles.button} btn btn-success`}>Создать</button>
+      <div className={styles.container}>
+        <label className="form-label" htmlFor="">
+          Введите название задачи
+        </label>
+        <input type="text" className="form-control" />
+      </div>
+      <button className={`${styles.button} btn btn-success`}>Создать</button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
