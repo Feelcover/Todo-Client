e9d50e045c1c3c999c62d9befc6c71db3d13f645
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-20 17:23:28 +0300

    запрос и экшен для получения всех задач

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,17 @@ import axios from "axios";
 import { ITodo, ITodoState } from "../types/types";
 
 export class TodoApi {
-    static async createTodo(todo:Partial<ITodo>):Promise<ITodoState[]>{
-        const res = await axios.post('http://localhost:3000/todos', todo);
-        return res.data;
-    }
+  static async getTodos(): Promise<ITodoState[]> {
+    const res = await axios.get("http://localhost:3000/todos");
+    return res.data;
+  }
 
-    static async deleteTodo(id:string):Promise<void>{
-        await axios.delete(`http://localhost:3000/todos/${id}`);
-    }
+  static async createTodo(todo: Partial<ITodo>): Promise<ITodoState[]> {
+    const res = await axios.post("http://localhost:3000/todos", todo);
+    return res.data;
+  }
+
+  static async deleteTodo(id: string): Promise<void> {
+    await axios.delete(`http://localhost:3000/todos/${id}`);
+  }
 }
-
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,5 +1,11 @@
 import { TodoActionTypes } from "../../types/types"
 
+export const getTodos = () => {
+    return {
+        type:TodoActionTypes.GET_TODOS,
+    }
+}
+
 export const createTodo = (payload: string) => {
     return {
         type:TodoActionTypes.CREATE_TODO,
