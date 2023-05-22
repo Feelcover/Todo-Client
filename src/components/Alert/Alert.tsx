1ae5b4eb073ff0115cccc689763ac62e8c08f1c1
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-22 17:56:32 +0300

    Alert

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { IAlertProps } from '../../types/types'
-import styles from "./Alert.module.css"
+import React from "react";
+import { useDispatch } from "react-redux";
+import { hideAlert } from "../../redux/actions";
+import { IAlertProps } from "../../types/types";
+import styles from "./Alert.module.css";
 
-const Alert = ({props}:IAlertProps) => {
-    const dispatch = useDispatch();
-    const handlerAlert = () => {
-        
-    }
+const Alert = ({ props }: IAlertProps) => {
+  const dispatch = useDispatch();
+  const handlerAlertClose = () => {
+    dispatch(hideAlert());
+  };
   return (
-    <div className={`${styles.alertWrapper} alert-${props.alertStatus}`}> 
-        {props.alertText}
-        <button className={`${styles.alertBtn} btn-close`}/>
+    <div className={`${styles.alertWrapper} alert-${props.alertStatus}`}>
+      {props.alertText}
+      <button
+        className={`${styles.alertBtn} btn-close`}
+        onClick={handlerAlertClose}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Alert
+export default Alert;
